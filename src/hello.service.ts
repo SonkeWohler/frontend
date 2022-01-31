353f904244ce5e2f4c1aa636968eb5294cfa26a8
sun <sonke.a.wohler@gmail.com>
2022-02-01 00:45:20 +0200

    fix url for server

--- src/hello.service.ts
+++ src/hello.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HelloService {
 
-  private url = "127.0.0.1:5000/hello"
+  private url = "http://127.0.0.1:5000/hello"
 
   constructor( private http: HttpClient,) { }
 
